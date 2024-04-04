069f1854f0e99f3cab60b967e47d30296956a548
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-04 17:09:25 +0700

    penambahan for dan menerima elemen array

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -20,7 +20,11 @@ void input()
     cout << "\n---------------------\n";
     cout << "Enter array elements \n";
     cout << "---------------------\n";
-
+    for (i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
 }
 
 int main()
