ae73c66da08ec5a25a6b20dbab4fe536e79a4dd0
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-04 17:07:21 +0700

    menampilkan memasukkan elemen array

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -16,6 +16,11 @@ void input()
         else
             cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
+
+    cout << "\n---------------------\n";
+    cout << "Enter array elements \n";
+    cout << "---------------------\n";
+
 }
 
 int main()
