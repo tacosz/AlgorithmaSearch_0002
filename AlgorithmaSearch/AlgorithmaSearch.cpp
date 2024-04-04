fddd1e88d6b4468d1a5e4cb1db5cb20dd291d3fc
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-04 17:25:52 +0700

    penambahan menampilkan array dari for

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -63,7 +63,8 @@ void LinearSearch()
 void display()
 {
     cout << "Array elements are: ";
-    for (i = 0; i < n; i++);
+    for (i = 0; i < n; i++)
+        cout << arr[i] << " ";
 }
 
 int main()
