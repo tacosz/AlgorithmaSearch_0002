3f00d10c57fb3a652fa9d03c2b9eacce0a29eed6
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-04 17:05:06 +0700

    penambahan else dan menampilkan minimal dan maksimal elemen array

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -13,6 +13,8 @@ void input()
         cin >> n;
         if ((n > 0) && (n <= 20))
             break;
+        else
+            cout << "\nArray should have minimum 1 and maximum 20 elements.\n\n";
     }
 }
 
