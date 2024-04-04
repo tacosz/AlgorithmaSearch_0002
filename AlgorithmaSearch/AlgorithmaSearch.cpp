137b0c7a1142e852006a4344dcb051f80fc51ef8
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-04 17:01:39 +0700

    menampilkan dan menerima elemen angka di array

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -5,6 +5,15 @@ int arr[20];
 int n;
 int i;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Enter the number of element in the array: ";
+        cin >> n;
+    }
+}
+
 int main()
 {
     
