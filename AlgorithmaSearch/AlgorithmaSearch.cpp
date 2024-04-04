6417816e736b3db677f12eaba437033c83c04cdb
tacosz <nabilnasruddin.offc@gmail.com>
2024-04-04 17:29:42 +0700

    penambahan perulangan do while

--- AlgorithmaSearch/AlgorithmaSearch.cpp
+++ AlgorithmaSearch/AlgorithmaSearch.cpp
@@ -45,7 +45,7 @@ void LinearSearch()
             ctr++;
             if (arr[i] == item)
             {
-                cout << "\n" << item << "found at position" << (i + 1) << endl; //step 6
+                cout << "\n" << item << " found at position " << (i + 1) << endl; //step 6
                 break;
             }
             i++; //step 4
